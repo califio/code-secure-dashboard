5a9f26e6d8a3feeac2b3b663478e952eb387733d
0xduo <buianhduong9x@gmail.com>
2025-01-21 16:24:55 +0700

    add jira integration ui

--- code-secure-ui/src/app/pages/project/project/setting/integration/jira/jira.component.ts
+++ code-secure-ui/src/app/pages/project/project/setting/integration/jira/jira.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {ButtonDirective} from '../../../../../../shared/ui/button/button.directive';
 import {ReactiveFormsModule} from '@angular/forms';
+import {ComingSoonComponent} from '../../../../../../shared/ui/coming-soon/coming-soon.component';
 
 @Component({
   selector: 'jira-integration',
   standalone: true,
   imports: [
     ButtonDirective,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ComingSoonComponent
   ],
   templateUrl: './jira.component.html',
   styleUrl: './jira.component.scss'
